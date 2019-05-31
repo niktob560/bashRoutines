d7de8b0aa9c9897a1fdfca9d238a1fc5917b13ff
niktob560 <niktob560@gmail.com>
2019-05-31 22:34:18 +0300

    Volume script update

--- changeVol.sh
+++ changeVol.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
+
+notification_id="991049"
+
 if [ $1 == 0 ]; then
 	pactl set-sink-mute @DEFAULT_SINK@ toggle
 else
 	pactl set-sink-mute @DEFAULT_SINK@ off
 	pactl set-sink-volume @DEFAULT_SINK@ $1
 fi
-vol=0
-muted=$(pactl list sinks | rg Mute | awk '{ print $2 }')
-vol=$(pactl list sinks | rg Volume: | awk -F '/' '{ print $4 }' | rg \[\\d] | tail -1)
+
+var=$(pactl list sinks | rg bluetooth);
+
+if [[ $var != "" ]]; then
+	vol=$(pactl list sinks | rg -A 2 -B 8 "bluetooth" | rg "Громкость" | awk -F '/' '{print $4}');
+	muted=$(pactl list sinks | rg -A 2 -B 8 "bluetooth" | rg "Звук выключен" | awk '{print $3}' | head -1)
+else
+	vol=$(pactl list sinks | rg "Громкость:" | head -1 | awk -F '/' '{print $4}')
+	muted=$(pactl list sinks | rg "Звук выключен" | awk '{print $3}' | head -1)
+fi
+
+#echo $vol
 vol=${vol::-2}
+#echo $vol
+vol=$(($vol+0))
 icon=""
-if [[ $muted == 'yes' ]]; then
-	icon="notification-audio-volume-muted"
+if [[ $muted == 'yes' || $vol == 0 ]]; then
+	icon="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg"
+	#iconn="audio-volume-muted.png"
 	vol=0
 else 
 	if [ "$vol" -lt "33" ]; then
-		icon="notification-audio-volume-low"
+		icon="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
 	else 
 		if [[ $vol -lt 66 ]]; then
-			icon="notification-audio-volume-medium"
+			icon="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-medium.svg"
 		else
-			icon="notification-audio-volume-high"
+			icon="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
 		fi
 	fi
 fi
-notify-send " " -i $icon -h int:value:$(echo $vol) -h string:synchronous:volume 
+bar=$(seq -s "─" $(($vol/5)) | sed 's/[0-9]//g')
+dunstify "$vol""     ""$bar" -i "$icon" -t 1000 -h int:value:"$vol" -h string:synchronous:"$bar" -r "$notification_id"
+#notify-send " " -i $icon -h int:value:$(echo $vol) -h string:synchronous:volume 
